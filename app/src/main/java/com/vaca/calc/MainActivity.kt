51b606014a9b4656e3929a1d160f693808a75698
李光浩 <liguanghao@viatomtech.com>
2021-06-02 14:33:17 +0800

    hidfsgdsfg

--- app/src/main/java/com/vaca/calc/MainActivity.kt
+++ app/src/main/java/com/vaca/calc/MainActivity.kt
@@ -6,6 +6,34 @@ import androidx.appcompat.app.AppCompatActivity
 import net.objecthunter.exp4j.Expression
 import net.objecthunter.exp4j.ExpressionBuilder
 
+/*
+abs: absolute value
+acos: arc cosine
+asin: arc sine
+atan: arc tangent
+cbrt: cubic root
+ceil: nearest upper integer
+cos: cosine
+cosh: hyperbolic cosine
+exp: euler's number raised to the power (e^x)
+floor: nearest lower integer
+log: logarithmus naturalis (base e)
+log2: logarithm to base 2
+log10: logarithm to base 10
+sin: sine
+sinh: hyperbolic sine
+sqrt: square root
+tan: tangent
+tanh: hyperbolic tangent
+signum: signum of a value
+Addition: '2 + 2'
+Subtraction: '2 - 2'
+Multiplication: '2 * 2'
+Division: '2 / 2'
+Exponential: '2 ^ 2'
+Unary Minus,Plus (Sign Operators): '+2 - (-2)'
+Modulo: '2 % 2'
+*/
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
