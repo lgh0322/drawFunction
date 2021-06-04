47b169a9340edf1e4b694b64210de5f4c9aec90d
李光浩 <liguanghao@viatomtech.com>
2021-06-04 17:43:14 +0800

    sdv

--- app/src/main/java/com/vaca/calc/MainActivity.kt
+++ app/src/main/java/com/vaca/calc/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.vaca.calc
 
 import android.os.Bundle
+import android.os.IBinder
 import android.os.Vibrator
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
 import com.vaca.calc.databinding.ActivityMainBinding
 import net.objecthunter.exp4j.Expression
 import net.objecthunter.exp4j.ExpressionBuilder
@@ -15,6 +20,7 @@ import java.lang.Exception
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     val validLineArray:MutableList <Line> = ArrayList()
     var vibrator: Vibrator? = null
+    val funString = MutableLiveData<String>()
     lateinit var binding:ActivityMainBinding
 
     lateinit var myFun:Expression
@@ -88,6 +94,61 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
 
+
+
+
+
+
+
+
+    override fun dispatchTouchEvent(me: MotionEvent): Boolean {
+        if (me.action == MotionEvent.ACTION_DOWN) {  //把操作放在用户点击的时候
+            val v = currentFocus //得到当前页面的焦点,ps:有输入框的页面焦点一般会被输入框占据
+            if (isShouldHideKeyboard(v, me)) { //判断用户点击的是否是输入框以外的区域
+                hideKeyboard(v!!.windowToken) //收起键盘
+            }
+        }
+        return super.dispatchTouchEvent(me)
+    }
+
+
+
+
+    private fun isShouldHideKeyboard(v: View?, event: MotionEvent): Boolean {
+        if (v != null && v is EditText) {  //判断得到的焦点控件是否包含EditText
+            val l = intArrayOf(0, 0)
+            v.getLocationInWindow(l)
+            val left = l[0]
+            //得到输入框在屏幕中上下左右的位置
+            val top = l[1]
+            val bottom = top + v.getHeight()
+            val right = left + v.getWidth()
+            return if (event.x > left && event.x < right && event.y > top && event.y < bottom) {
+                // 点击位置如果是EditText的区域，忽略它，不收起键盘。
+                false
+            } else {
+                true
+            }
+        }
+        // 如果焦点不是EditText则忽略
+        return false
+    }
+
+
+    /**
+     * 获取InputMethodManager，隐藏软键盘
+     * @param token
+     */
+    private fun hideKeyboard(token: IBinder?) {
+        if (token != null) {
+            val im: InputMethodManager =
+                getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            im.hideSoftInputFromWindow(token, InputMethodManager.HIDE_NOT_ALWAYS)
+        }
+    }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?)  {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
@@ -101,105 +162,107 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.x4.setOnClickListener(this)
 
 
+        funString.observe(this,{
+            initFun(it)
+            binding.func.setText(it)
+            validLineArray.clear()
+            for(xIndex in 0 until xCount){
+                for(yIndex in 0 until yCount){
 
+                    val tempBigX=lX+xIndex*deltaX+deltaX
+                    val tempSmallX=lX+xIndex*deltaX
+                    pointXset= doubleArrayOf(tempSmallX,tempBigX,tempBigX, tempSmallX)
 
-
-//        initFun("x ^ 2 + y ^ 2 -12.01")
-
-        initFun("y-sin(3*x)")
-
-
-        for(xIndex in 0 until xCount){
-            for(yIndex in 0 until yCount){
-
-                val tempBigX=lX+xIndex*deltaX+deltaX
-                val tempSmallX=lX+xIndex*deltaX
-                pointXset= doubleArrayOf(tempSmallX,tempBigX,tempBigX, tempSmallX)
-
-                val tempSmallY=yIndex*deltaY+dY
-                val tempBigY=yIndex*deltaY+dY+deltaY
-                pointYset= doubleArrayOf(tempBigY, tempBigY, tempSmallY, tempSmallY)
+                    val tempSmallY=yIndex*deltaY+dY
+                    val tempBigY=yIndex*deltaY+dY+deltaY
+                    pointYset= doubleArrayOf(tempBigY, tempBigY, tempSmallY, tempSmallY)
 
 
 
 
-            /*    val pointSign=IntArray(4){
-                    0
-                }*/
+                    /*    val pointSign=IntArray(4){
+                            0
+                        }*/
 
-                var signTotal=0;
+                    var signTotal=0;
 
-                for(vertexIndex in 0 until 4){
-                    pointValue[vertexIndex]=getValue(pointXset[vertexIndex],pointYset[vertexIndex])
+                    for(vertexIndex in 0 until 4){
+                        pointValue[vertexIndex]=getValue(pointXset[vertexIndex],pointYset[vertexIndex])
 //                    pointSign[vertexIndex]=sign(pointValue[vertexIndex])
-                    signTotal+=(1.shl(3-vertexIndex))*sign(pointValue[vertexIndex])
+                        signTotal+=(1.shl(3-vertexIndex))*sign(pointValue[vertexIndex])
+                    }
+
+
+                    when(signTotal){
+                        1->{
+                            validLineArray.add(case1(2,3,0))
+                        }
+                        2->{
+                            validLineArray.add(case1(1,2,3))
+                        }
+                        4->{
+                            validLineArray.add(case1(0,1,2))
+                        }
+                        8->{
+                            validLineArray.add(case1(3,0,1))
+                        }
+
+                        7->{
+                            validLineArray.add(case1(3,0,1))
+                        }
+                        11->{
+                            validLineArray.add(case1(0,1,2))
+                        }
+
+                        13->{
+                            validLineArray.add(case1(1,2,3))
+                        }
+                        14->{
+                            validLineArray.add(case1(2,3,0))
+                        }
+
+                        //------------------------------------------case 2
+                        3->{
+                            validLineArray.add(case2(1,2,3,0))
+
+                        }
+
+                        6->{
+                            validLineArray.add(case2(0,1,2,3))
+                        }
+
+                        9->{
+                            validLineArray.add(case2(2,3,0,1))
+                        }
+
+
+                        12->{
+                            validLineArray.add(case2(3,0,1,2))
+                        }
+
+
+
+                    }
+
+
+
                 }
-
-
-                when(signTotal){
-                    1->{
-                        validLineArray.add(case1(2,3,0))
-                    }
-                    2->{
-                        validLineArray.add(case1(1,2,3))
-                    }
-                    4->{
-                        validLineArray.add(case1(0,1,2))
-                    }
-                    8->{
-                        validLineArray.add(case1(3,0,1))
-                    }
-
-                    7->{
-                        validLineArray.add(case1(3,0,1))
-                    }
-                    11->{
-                        validLineArray.add(case1(0,1,2))
-                    }
-
-                    13->{
-                        validLineArray.add(case1(1,2,3))
-                    }
-                    14->{
-                        validLineArray.add(case1(2,3,0))
-                    }
-
-                    //------------------------------------------case 2
-                    3->{
-                        validLineArray.add(case2(1,2,3,0))
-
-                    }
-
-                    6->{
-                        validLineArray.add(case2(0,1,2,3))
-                    }
-
-                    9->{
-                        validLineArray.add(case2(2,3,0,1))
-                    }
-
-
-                    12->{
-                        validLineArray.add(case2(3,0,1,2))
-                    }
-
-
-
-                }
-
-
-
             }
-        }
+
+            binding.graph.drawLineArray=validLineArray
+
+            binding.graph.invalidate()
+
+        })
+
+
+
 
 
         binding.graph.initCor(lX,rX,uY,dY)
 
-        binding.graph.drawLineArray=validLineArray
 
-        Log.e("fuckaaaaaa",validLineArray.size.toString())
-        binding.graph.invalidate()
-
+        funString.postValue("y ^2 +x^2-5")
 
 
 
@@ -255,5 +318,24 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         vibrator?.vibrate(30)
+        when(v?.id){
+            R.id.x1->{
+                if( binding.group.visibility!=View.VISIBLE){
+                    binding.group.visibility=View.VISIBLE
+                }else{
+                    binding.group.visibility=View.GONE
+                }
+
+            }
+
+            R.id.x2->{
+
+            }
+
+            R.id.x4->{
+                funString.postValue(binding.func.text.toString())
+            }
+        }
+
     }
 }
